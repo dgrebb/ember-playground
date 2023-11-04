b49bf29b8894b74d1f7ce9c1ae914095b4755aa8
Dan Grebb <dan@dgrebb.com>
2023-11-04 12:45:38 -0400

    build(project): renames project

--- config/environment.js
+++ config/environment.js
@@ -2,7 +2,7 @@
 
 module.exports = function (environment) {
   const ENV = {
-    modulePrefix: 'ember-quickstart',
+    modulePrefix: 'ember-playground',
     environment,
     rootURL: '/',
     locationType: 'history',
